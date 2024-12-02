9237ca494c73b0971e114295f7ca89823a976575
BoyceWhite <boycewhite2002@gmail.com>
2024-12-01 17:15:35 -0700

    Added Test Data and BOW

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import string
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
-data = pd.read_csv('IMDB Dataset.csv')
+data = pd.read_csv('TEST Dataset.csv')
+# data = pd.read_csv('IMDB Dataset.csv')
+
 reviews = data['review']
 
 #Removing Punctuation and printing an example
@@ -10,5 +12,9 @@ print("\n PUNCTUATION INTACT: \n", reviews[0])
 reviews = reviews.str.lower().str.strip().str.translate(str.maketrans('', '', string.punctuation))
 print("\n PUNCTUATION REMOVED: \n", reviews[0])
 
+#Creating Bag Of Words
+vectorizer = CountVectorizer(stop_words=None)
+vectorizer.fit(reviews)
 
+print(vectorizer.vocabulary_)
 
